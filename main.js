bb6e2941e4399a9b70dff7c5208af53c39693211
dmarie777 <deimarie777@gmail.com>
2024-06-15 20:32:07 -0500

    Added viewTimeline animation in JS

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const progressbar = document.querySelector('#progress');
+/*const progressbar = document.querySelector('#progress');
 
 progressbar.style.transformOrigin = '0% 50%';
 
@@ -13,4 +13,21 @@ progressbar.animate(
             source: document.documentElement,
         })
     }
-)
+)*/
+
+// const images = document.querySelectorAll('.conatiner-box');
+// images.forEach((image) => {
+//     image.animate(
+//         [
+//             { opacity:0, clipPath: 'inset(45% 20% 45% 20%)', offset:0 },
+//             { opacity: 1, clipPath: 'inset(0)', offset:0.5 }
+//         ], 
+//         {
+//             fill: 'both',
+//             timeline: new ViewTimeline({
+//                 subject: image,
+//                 axis: 'block',
+//             })
+//         }
+//     )
+// })
